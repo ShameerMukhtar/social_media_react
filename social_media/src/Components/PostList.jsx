0574e9b22d87e397e467c7e0c7adae3606472aa3
Shameer Mukhtar <94970687+ShameerMukhtar@users.noreply.github.com>
2024-07-25 19:01:48 +0500

    Loading Animation/useEffect

--- social_media/src/Components/PostList.jsx
+++ social_media/src/Components/PostList.jsx
@@ -1,31 +1,37 @@
-import { useContext, useState } from "react";
+import { useContext, useEffect,useState } from "react";
 import Post from "./Post";
 import { PostListContext } from "../Store/post-list-store";
 import CheckPosts from "./CheckPosts";
+import LoadingSpinner from "./LoadingSpinner";
 
 
 
 const PostList =({selectedTab})=>{
   // Getting data from store using useContext;
-const [FetchStatus,SetFetchStatus]=useState(false);
 
+const [fetching,setFetching]=useState(false);
 
 const {postList,initial_Posts}=useContext(PostListContext);
 
 // handle  Fetch Posts
-if(!FetchStatus){
-  fetch('https://dummyjson.com/posts').then((response)=>response.json()).then((data)=>initial_Posts(data.posts)).catch((error)=>{
-    console.log("Something Wrong"+error);
-  })
-SetFetchStatus(true);
-}
+useEffect(()=>{
+setFetching(true);
+
+  fetch('https://dummyjson.com/posts').then((response)=>response.json()).then((data)=>{initial_Posts(data.posts)
+  setFetching(false)}
+  )
+  
+  
+},[])
+
 
 
 
 
 // map the postList and share it with Post component as props 
 return <>
-{postList.length===0 && selectedTab==="Home"?<CheckPosts ></CheckPosts>:null}
+{fetching  && <LoadingSpinner/>}
+{!fetching && postList.length===0 && <CheckPosts ></CheckPosts>}
 {postList.map((post)=>{
   // console.log(post);
   return <Post postList={post} key={post.id}></Post>
